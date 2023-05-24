1b83ad38b38f9a11e835c9fd65db895bdf4eed45
Eunice Koh <eunicekoh46@gmail.com>
2023-05-24 12:26:54 +0800

    feat: Implement sign up, login, update account, and logout functionality
    This commit adds the following features:
    1) Sign up functionality allowing users to create an account using their email and password.
    2) Login functionality to authenticate users with their correct email and password.
    3) Update account functionality enabling users to change their password after logging in.
    4) Logout functionality to allow users to securely sign out of their account.
    
    These features enhance the user experience and improve the security of the application.

---
+++ src/components/Dashboard.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import { Card, Button, Alert } from "react-bootstrap";
+import { useAuth } from "../contexts/AuthContext";
+import { Link, useNavigate } from "react-router-dom";
+
+export default function Dashboard() {
+  const [error, setError] = useState("");
+  const { currentUser, logout } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Check if currentUser is null or not
+    if (!currentUser) {
+      // Redirect to the login page or handle the unauthorized state
+      navigate("/login");
+    }
+  }, [currentUser, navigate]);
+
+  async function handleLogout() {
+    setError("");
+
+    try {
+      await logout();
+      navigate("/login");
+    } catch {
+      setError("Failed to log out");
+    }
+  }
+
+  if (!currentUser) {
+    // Show a loading indicator or redirect to the login page
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Profile</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <strong>Email:</strong> {currentUser.email}
+          <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
+            Update Profile
+          </Link>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        <Button variant="link" onClick={handleLogout}>
+          Log Out
+        </Button>
+      </div>
+    </>
+  );
+}
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyATyplCEKUv39r2o5MLSrP4Ib8xYsV8sgU",
+  authDomain: "midnights-capriuscats.firebaseapp.com",
+  projectId: "midnights-capriuscats",
+  storageBucket: "midnights-capriuscats.appspot.com",
+  messagingSenderId: "823684049137",
+  appId: "1:823684049137:web:5570aa772fd4eaf69fe150",
+  measurementId: "G-M7XP6V5HK1",
+});
+
+export const auth = app.auth();
+export default app;
