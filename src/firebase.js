cdb6d294e3013bf9943f7fd24f2b181536e12891
catherinelng <122343294+catherinelng@users.noreply.github.com>
2023-05-27 15:56:52 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,6 @@
-/*
 import firebase from "firebase/compat/app"
 import "firebase/compat/auth"
 import "firebase/compat/firestore"
-*/
-import firebase from "firebase/app"
-import "firebase/auth"
-import "firebase/firestore"
 
 const app = firebase.initializeApp({
   apiKey: "AIzaSyATyplCEKUv39r2o5MLSrP4Ib8xYsV8sgU",
