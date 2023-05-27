e63cccc279d0fb629b9777fca6c780419bd74512
catherinelng <122343294+catherinelng@users.noreply.github.com>
2023-05-27 19:28:34 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,4 @@
 /*
-import firebase from "firebase/compat/app"
-import "firebase/compat/auth"
-import "firebase/compat/firestore"
-*/
 import { initializeApp } from 'firebase/app';
 const config = {
   apiKey: "AIzaSyATyplCEKUv39r2o5MLSrP4Ib8xYsV8sgU",
@@ -18,3 +14,22 @@ const app = initializeApp(config);
 
 export const auth = app.auth();
 export default app;
+*/
+
+import firebase from "firebase/compat/app"
+import "firebase/compat/auth"
+import "firebase/compat/firestore"
+
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyATyplCEKUv39r2o5MLSrP4Ib8xYsV8sgU",
+  authDomain: "midnights-capriuscats.firebaseapp.com",
+  projectId: "midnights-capriuscats",
+  storageBucket: "midnights-capriuscats.appspot.com",
+  messagingSenderId: "823684049137",
+  appId: "1:823684049137:web:5570aa772fd4eaf69fe150",
+  measurementId: "G-M7XP6V5HK1",
+});
+
+export const auth = app.auth();
+export default app;
+
