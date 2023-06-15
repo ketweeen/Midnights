0b5ef24373eefb0e05f3e0ab298eccb286f2d9e8
catherinelng <122343294+catherinelng@users.noreply.github.com>
2023-06-15 09:25:26 +0200

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,31 +1,26 @@
 import React, { useRef, useState } from "react";
 import { Form, Button, Card, Alert } from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-export default function Signup() {
+export default function ForgotPassword() {
   const emailRef = useRef();
-  const passwordRef = useRef();
-  const passwordConfirmRef = useRef();
-  const { signup } = useAuth();
+  const { resetPassword } = useAuth();
   const [error, setError] = useState("");
+  const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
 
   async function handleSubmit(e) {
     e.preventDefault();
 
-    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-      return setError("Passwords do not match");
-    }
-
     try {
+      setMessage("");
       setError("");
       setLoading(true);
-      await signup(emailRef.current.value, passwordRef.current.value);
-      navigate("/");
+      await resetPassword(emailRef.current.value);
+      setMessage("Check your inbox for further instructions");
     } catch {
-      setError("Failed to create an account");
+      setError("Failed to reset password");
     }
 
     setLoading(false);
@@ -33,31 +28,45 @@ export default function Signup() {
 
   return (
     <>
-      <Card>
+      <Card style={{backgroundColor: 'transparent', border: 0}}>
         <Card.Body>
-          <h2 className="text-center mb-4">Sign Up</h2>
+        <h2 className="text-center mb-4" 
+          style={{fontSize: 20, 
+                  color: "#bd9dee", 
+                  backgroundColor: "white", 
+                  opacity:0.9}}><b>- password reset -</b></h2>
           {error && <Alert variant="danger">{error}</Alert>}
+          {message && <Alert variant="success">{message}</Alert>}
           <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required />
+              {/* <Form.Label>Email</Form.Label> */}
+              <Form.Control type="email" ref={emailRef} required placeholder="Email" 
+              style={{borderRadius: 40, 
+                      height:"60px", 
+                      padding:20, 
+                      minWidth:300,
+                      marginBottom: 25}} />
             </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" ref={passwordRef} required />
-            </Form.Group>
-            <Form.Group id="password-confirm">
-              <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control type="password" ref={passwordConfirmRef} required />
-            </Form.Group>
-            <Button disabled={loading} className="w-100" type="submit">
-              Sign Up
-            </Button>
+            <div className="text-center">
+              <Button disabled={loading} className="w;10" type="submit" 
+              style={{backgroundColor: "#bd9dee",
+                      borderColor: "white",
+                      borderRadius: 25,
+                      height:35,
+                      fontSize: 15,
+                      minWidth: 100}}>
+                Reset Password
+              </Button>
+            </div>
           </Form>
+          <div className="w-100 text-center mt-3">
+            <Link style={{color: "white"}} to="/login">Login</Link>
+          </div>
         </Card.Body>
       </Card>
-      <div className="w-100 text-center mt-2">
-        Already have an account? <Link to="/login">Log In</Link>
+      <div className="w-100 text-center mt-2"
+       style={{color: "#efd5d1"}}>
+      <b>Need an account? <Link to="/signup" style={{color: "#efd5d1"}}>Sign Up</Link></b>
       </div>
     </>
   );
